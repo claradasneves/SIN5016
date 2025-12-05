5a8eb6cfc97cc0d2568169baae969d0f21d67411
Samuel Caetano <sam.kaetano@gmail.com>
2025-12-04 21:24:18 -0300

    feat: added placeholder main

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == '__main__':
+    # 1 - carrega dados
+    # 2 - divide em partição de treino/teste
+    # 3 - converte rótulos com one-hot-encoding
+    # 4 - aplica .fit() do modelo
