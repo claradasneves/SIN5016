ad5717754ad4f8046185b9534ef815a0b1321a32
Samuel Caetano <sam.kaetano@gmail.com>
2025-12-06 10:53:17 -0300

    feat: add lost_functions to single file

---
+++ utils/loss_functions.py
@@ -0,0 +1,47 @@
+"""
+Docstring for utils.loss_functions
+
+Funções de custo para os modelos
+"""
+
+import numpy as np
+
+def mae(y_true: np.array, y_pred: np.array):
+    return np.mean(np.abs(y_pred - y_true))
+
+def gradient_mae(y_true: np.array, y_pred : np.array):
+    """
+    Gradiente descendente da MAE
+    """
+    return np.sign(y_pred - y_true)
+
+def mse(y_true: np.array, y_pred: np.array):   
+    return np.mean((y_pred - y_true)**2)
+
+def gradient_mse(y_true: np.array, y_pred: np.array):
+    """
+    Gradiente descendente da MSE
+    """
+
+    return 2*np.mean(y_pred - y_true)
+
+def entropia_cruzada(
+        y_true: np.array, 
+        y_pred: np.array, 
+    ):
+    """
+    Função de custo multiclasse (entropia cruzada)
+    """
+    # np.clip previne log(0)
+    y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+    
+    loss = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
+
+    return loss
+
+def gradient_cross_entropy(y_true: np.array, y_pred: np.array):
+    """
+    Gradiente da Entropia Cruzada (Negativo da Log-Verossimilhança).
+    """
+    return -y_true / (1 + y_pred)
+
