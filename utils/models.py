f87dfdf09a1e32022a3755627823f556e3078d49
Samuel Caetano <sam.kaetano@gmail.com>
2025-12-06 13:01:26 -0300

    feat: add logistic regression

--- utils/models.py
+++ utils/models.py
@@ -144,5 +144,96 @@ class MLP():
 
 class RegLog():
     """ regressão logística"""
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            num_features,
+        ):
+        self.M = num_features
+
+        W = np.random.randn(self.M, 1)
+        self.W = np.insert(W, 0, 1, axis=1) # add bias
+
+    def gradient_descent(
+            self,
+            cost_function,
+            X, y,
+            alpha=0.001, 
+            tol=0.01,
+            batch_size=16):
+        """"
+        Args:
+            x: input vector
+            steps: qtdade de passos para andar na direção do gradiente
+            alpha: escalar que reduz o tamanho do vetor gradiente, isso serve para eu saber a direção, mas andar passos curtos na direção indicada
+            tol: o tamanho mínimo que eu espero que meu gradiente chegue. Isso indica o tamanho de tolerância para atualizações do gradiente.
+        
+        Returns
+            o ponto mínimo local, no contexto dos hiperparâmetros.
+        """
+
+        N = batch_size
+        losses = []
+
+        for start in range(0, N, batch_size):
+
+            batch_x = X[start:start+batch_size]
+            batch_y = y[start:start+batch_size]
+            
+            y_pred = self.predict(batch_x)
+
+            loss = cost_function(batch_y, y_pred)
+            losses.append(loss)
+
+            # shape: (N, m).T -> (m, N) x (N, k) -> (m, k)
+            dEdW = batch_x.T.dot(y_pred - batch_y)
+            
+            self.W -= alpha * dEdW
+        
+        losses = np.array(losses)
+
+        return losses, dEdW
+    
+    def predict(self, X: np.array):
+        """Modelo da Regressão Logística"""
+        
+        logits = X.dot(self.W)
+        logistica = (np.e**logits) / (1 + np.e**logits)
+        
+        return logistica
+    
+    def fit(
+        self,
+        cost_function,
+        X_train, y_train, X_test, y_test,
+        optimizer='GD',
+        epochs=1000, 
+        learning_rate=0.001,
+        tol=0.001,    
+        ):
+
+        history_loss = []
+        for epoch in range(epochs):
+
+            if optimizer == 'GD':
+                losses, dEdW = self.gradient_descent(
+                    X=X_train, 
+                    y=y_train,
+                    cost_function=cost_function,
+                    alpha=learning_rate,
+                )
+
+            # Avaliação Final
+            y_pred_val = self.predict(X_test)
+
+            val_loss = cost_function(y_test, y_pred_val)
+            history_loss.append(val_loss)
+
+            if np.linalg.norm(dEdW) < tol: break
+
+            print(f'Avg training loss={np.mean(losses)}')
+            print(f'Avg validation loss={np.mean(history_loss)}')
+
+        history_loss = np.array(history_loss)
+
+        return history_loss
+    
